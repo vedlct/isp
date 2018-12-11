640919e4b8f7fee96d76b2491e635c782b767941
Farzad <farzadrahman59@gmail.com>
2018-12-11 14:12:25 +0600

    Farzad

--- resources/views/bill/cable/pdf.blade.php
+++ resources/views/bill/cable/pdf.blade.php
@@ -4,45 +4,9 @@
     <title>Invoice</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">--}}
+
     <style>
-        /* body {
-             background: #ddd none repeat scroll 0 0;
-         }*/
 
-
-        /*
-        img {
-            width: 80px;
-
-        }
-        .versity_name span {
-            color: red;
-        }
-
-        .application h3 {
-            color: red;
-            font-size: 25px;
-            margin-bottom: 30px;
-            text-align: center;
-            text-transform: uppercase;
-        }
-
-        .versity_name h2 {
-            font-size: 37px;
-            margin-left: 18px;
-        }
-        .application p {
-            margin: 0;
-            padding: 0;
-        }
-        .photo > p {
-            border: 1px solid;
-            height: 122px;
-            margin-top: 5px;
-            text-align: center;
-            width: 110px;
-        }*/
 
         table, th, td {
             border: 1px solid black;
@@ -61,28 +25,13 @@
             background-color: #ddd;
 
         }
-
-
-
-    </style>
-    {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>--}}
-    {{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>--}}
-
-    <style>
-
-        @font-face {
-            font-family: 'Helvetica', sans-serif;
-            src: url('public/fonts/HelveticaLt_1.ttf');
-        }
-
-        /*<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300" rel="stylesheet">*/
         body{
-            font-family: 'Helvetica', sans-serif;
             font-size: 11px;
         }
 
 
     </style>
+
 
 </head>
 
@@ -91,18 +40,12 @@
 </style>
 
 <body style="background: #fff ">
+
+@for($i=1;$i<6;$i++)
+
 <div class="structure" style="margin-left: 20px;">
+    {{--Test--}}
 
-
-
-
-
-
-
-
-
-
-        {{--Test--}}
         <table style="width: 100%; border:none;">
 
             <tr>
@@ -112,13 +55,10 @@
 
                         <tr style="margin: 0px;width: 100%">
                             <td style="border: none;padding: 0px;" >
-                            <img style="margin:0px" width="100px" height="60px" src="{{url('public/logo/TCL_logo.png')}}" alt="">
                             </td>
                             <td style="text-align: center; border: none; ">
                                 <h1 style="">
-                                    INVOICE<br>
-                                    {{$date}}
-
+                                    INVOICE-{{$date}}
                                 </h1>
                             </td>
                             <td style="border: none;">
@@ -145,66 +85,43 @@
 
                             <td style="border: none;">
                                 <div style="margin-left: 20%">
-                                <p><h3 style="color: #0476BD">{{strtoupper($client->clientFirstName)}} {{strtoupper($client->clientLastName)}}</h3></p>
+                                    <p><h3 style="color: #0476BD">{{strtoupper($client->clientFirstName)}} {{strtoupper($client->clientLastName)}}</h3></p>
 
-                                <p style="margin-top: -10px">
+                                    <p style="margin-top: -10px">
 
-                                    E: {{$client->email}} <br>
-                                    P: {{$client->phone}}<br>
-                                    Address : {{$client->address }}<br>
+                                        E: {{$client->email}} <br>
+                                        P: {{$client->phone}}<br>
+                                        Address : {{$client->address }}<br>
 
 
-                                </p>
+                                    </p>
                                 </div>
                             </td>
 
                         </tr>
-                        {{--<tr>--}}
 
-                            {{--<td style="width: 35%;border: none ">--}}
-
-
-
-
-                            {{--</td>--}}
-                            {{--<td style="width: 40%;border: none "></td>--}}
-
-                            {{--<td style="border: none;width: 25%;">--}}
-                                {{--<img style="margin-left: 30px" width="198px" height="148px" src="{{url('public/logo/TCL_logo.png')}}" alt="">--}}
-                            {{--</td>--}}
-
-                            {{--<td style="width: 40%; border: none;">--}}
-                                {{----}}
-
-
-                            {{--</td>--}}
-
-                        {{--</tr>--}}
                     </table>
-                    {{--<table style="width:100%; margin-top: 0;border: none;">--}}
-
-                      {{----}}
-                    {{--</table>--}}
 
 
-                <table border="0" style="width:100%;">
-                <tr style="background: #4682B4;color: white">
-                <td style="text-align: center;" colspan=""><b>Month</b></td>
-                <td style="text-align: center;" colspan=""><b>Prev Due</b></td>
-                <td style="text-align: center;" colspan=""><b>Due</b></td>
-                <td style="text-align: center;" colspan=""><b>Total</b></td>
-                </tr>
-                <tr style="">
-                <td style="text-align: center;" colspan=""></td>
-                <td style="text-align: center;" colspan=""><b>0</b></td>
-                <td style="text-align: center;" colspan=""><b>{{$client->price}}</b></td>
-                <td style="text-align: center;" colspan=""><b>{{$client->price}}</b></td>
-                </tr>
+
+                    <table border="0" style="width:100%;">
+                        <tr style="background: #4682B4;color: white">
+                            <td style="text-align: center;" colspan=""><b>Month</b></td>
+                            <td style="text-align: center;" colspan=""><b>Prev Due</b></td>
+                            <td style="text-align: center;" colspan=""><b>Due</b></td>
+                            <td style="text-align: center;" colspan=""><b>Total</b></td>
+                        </tr>
+                        <tr style="">
+                            <td style="text-align: center;" colspan=""></td>
+                            <td style="text-align: center;" colspan=""><b>0</b></td>
+                            <td style="text-align: center;" colspan=""><b>{{$client->price}}</b></td>
+                            <td style="text-align: center;" colspan=""><b>{{$client->price}}</b></td>
+                        </tr>
 
 
 
 
-                </table>
+                    </table>
 
                 </td>
 
@@ -214,8 +131,8 @@
 
             <tr>
                 <td style="width: 100%;border: none;">
-                {{--Client Copy--}}
-                    <hr style="border:2px solid dotted;margin-top: 20px">
+                    {{--Client Copy--}}
+                    <hr style="border:2px solid dotted;">
                     Client Copy
                     <table border="0" style="width:100%;">
                         <tr>
@@ -250,7 +167,7 @@
 
 
                     {{--Office Copy--}}
-                    <hr style="border:2px solid dotted;margin-top: 20px">
+                    <hr style="border:2px solid dotted;">
                     Office Copy
                     <table border="0" style="width:100%;">
                         <tr>
@@ -290,6 +207,12 @@
             </tr>
 
         </table>
+        {{--Test Ends--}}
+
+    @if($i%2==1)
+        <hr style="border:2px solid dotted;">
+    @endif
+
 
 
 
@@ -298,200 +221,10 @@
 
 
 
-        <hr style="border:2px solid dotted;margin-top: 20px">
 
-    <table style="width: 100%; border:none;">
+</div>
 
-        <tr>
-
-            <td  style="width: 100%;border: none;">
-                <table  style="width:100%; text-align: center; border: none;" >
-
-                    <tr style="margin: 0px;width: 100%">
-                        <td style="border: none;padding: 0px;" >
-                            <img style="margin:0px" width="100px" height="60px" src="{{url('public/logo/TCL_logo.png')}}" alt="">
-                        </td>
-                        <td style="text-align: center; border: none; ">
-                            <h1 style="">
-                                INVOICE<br>
-                                {{$date}}
-
-                            </h1>
-                        </td>
-                        <td style="border: none;">
-
-                        </td>
-
-                    </tr>
-
-                    <tr >
-                        <td style="border: none;">
-                            <p><h3 style="color: #0476BD;">{{strtoupper($company->companyTitle)}}</h3></p>
-
-                            <p>{{$company->companyAddress}}</p>
-
-                            <p style="margin-top: -10px">
-                                P: {{$company->companyPhone1}}, {{$company->companyPhone2}} <br>
-                                E: {{$company->companyEmail}}<br>
-                            </p>
-                        </td>
-
-                        <td style="text-align: center; border: none;">
-
-                        </td>
-
-                        <td style="border: none;">
-                            <div style="margin-left: 20%">
-                                <p><h3 style="color: #0476BD">{{strtoupper($client->clientFirstName)}} {{strtoupper($client->clientLastName)}}</h3></p>
-
-                                <p style="margin-top: -10px">
-
-                                    E: {{$client->email}} <br>
-                                    P: {{$client->phone}}<br>
-                                    Address : {{$client->address }}<br>
-
-
-                                </p>
-                            </div>
-                        </td>
-
-                    </tr>
-                    {{--<tr>--}}
-
-                    {{--<td style="width: 35%;border: none ">--}}
-
-
-
-
-                    {{--</td>--}}
-                    {{--<td style="width: 40%;border: none "></td>--}}
-
-                    {{--<td style="border: none;width: 25%;">--}}
-                    {{--<img style="margin-left: 30px" width="198px" height="148px" src="{{url('public/logo/TCL_logo.png')}}" alt="">--}}
-                    {{--</td>--}}
-
-                    {{--<td style="width: 40%; border: none;">--}}
-                    {{----}}
-
-
-                    {{--</td>--}}
-
-                    {{--</tr>--}}
-                </table>
-                {{--<table style="width:100%; margin-top: 0;border: none;">--}}
-
-                {{----}}
-                {{--</table>--}}
-
-
-                <table border="0" style="width:100%;">
-                    <tr style="background: #4682B4;color: white">
-                        <td style="text-align: center;" colspan=""><b>Month</b></td>
-                        <td style="text-align: center;" colspan=""><b>Prev Due</b></td>
-                        <td style="text-align: center;" colspan=""><b>Due</b></td>
-                        <td style="text-align: center;" colspan=""><b>Total</b></td>
-                    </tr>
-                    <tr style="">
-                        <td style="text-align: center;" colspan=""></td>
-                        <td style="text-align: center;" colspan=""><b>0</b></td>
-                        <td style="text-align: center;" colspan=""><b>{{$client->price}}</b></td>
-                        <td style="text-align: center;" colspan=""><b>{{$client->price}}</b></td>
-                    </tr>
-
-
-
-
-                </table>
-
-            </td>
-
-
-        </tr>
-
-
-        <tr>
-            <td style="width: 100%;border: none;">
-                {{--Client Copy--}}
-                <hr style="border:2px solid dotted;margin-top: 20px">
-                Client Copy
-                <table border="0" style="width:100%;">
-                    <tr>
-                        <td></td>
-                        <td>Name :</td>
-                        <td colspan="2">{{strtoupper($client->clientFirstName)}} {{strtoupper($client->clientLastName)}}</td>
-                        <td>Mem. No.</td>
-                        <td colspan="2">{{$client->clientSerial}}</td>
-                        <td>Tv No.</td>
-                        <td>{{$client->noOfTv}}</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>Address :</td>
-                        <td colspan="2">{{$client->address }}</td>
-                        <td>Con. Date</td>
-                        <td colspan="2">{{$client->conDate}}</td>
-                        <td>C.C.</td>
-                        <td>0</td>
-
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>Date</td>
-                        <td colspan="2"></td>
-                        <td colspan="2">Paid:</td>
-                        <td colspan="2">Due:</td>
-                        <td>{{$client->phone}}</td>
-                    </tr>
-
-                </table>
-
-
-                {{--Office Copy--}}
-                <hr style="border:2px solid dotted;margin-top: 20px">
-                Office Copy
-                <table border="0" style="width:100%;">
-                    <tr>
-                        <td></td>
-                        <td>Name :</td>
-                        <td colspan="2">{{strtoupper($client->clientFirstName)}} {{strtoupper($client->clientLastName)}}</td>
-                        <td>Mem. No.</td>
-                        <td colspan="2">{{$client->clientSerial}}</td>
-                        <td>Tv No.</td>
-                        <td>{{$client->noOfTv}}</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>Address :</td>
-                        <td colspan="2">{{$client->address }}</td>
-                        <td>Con. Date</td>
-                        <td colspan="2">{{$client->conDate}}</td>
-                        <td>C.C.</td>
-                        <td>0</td>
-
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>Date</td>
-                        <td colspan="2"></td>
-                        <td colspan="2">Paid:</td>
-                        <td colspan="2">Due:</td>
-                        <td>{{$client->phone}}</td>
-                    </tr>
-
-                </table>
-
-
-            </td>
-
-
-        </tr>
-
-    </table>
-
-
-
-
-    </div>
+@endfor
 
 </body>
 </html>
